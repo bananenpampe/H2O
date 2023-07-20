faca8647a9853114eaf787faf0361374395b5367
Matthias Kellner <bananenpampe@t-online.de>
2023-07-20 13:40:24 +0200

    adding todos removing detach

--- model/dataset/dataset_helpers.py
+++ model/dataset/dataset_helpers.py
@@ -23,27 +23,3 @@ def get_global_unique_species(frames):
         species.extend(frame.get_atomic_numbers())
 
     return np.unique(species)
-
-
-def _detach_all_blocks(tensor_map):
-    blocks = []
-    for _, block in tensor_map.items():
-        new_block = equistore.TensorBlock(
-            values=block.values.detach(),
-            samples=block.samples,
-            components=block.components,
-            properties=block.properties,
-        )
-
-        for parameter in block.gradients_list():
-            gradient = block.gradient(parameter)
-
-            new_block.add_gradient(
-                parameter=parameter,
-                data=gradient.data.detach(),
-                samples=gradient.samples,
-                components=gradient.components,
-            )
-
-        blocks.append(new_block)
-    return equistore.TensorMap(tensor_map.keys, blocks)
