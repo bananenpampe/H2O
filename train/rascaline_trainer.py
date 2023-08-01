4e98c0d219f5a3f6996903b5bcaa49b95173f5e9
Matthias Kellner <bananenpampe@t-online.de>
2023-08-01 16:52:22 +0200

    changes for regularization

--- train/rascaline_trainer.py
+++ train/rascaline_trainer.py
@@ -11,14 +11,14 @@ from nn.loss import EnergyForceLoss
 
 class BPNNRascalineModule(pl.LightningModule):
     
-    def __init__(self, example_tensormap, energy_transformer, lambda_=1e-03):
+    def __init__(self, example_tensormap, energy_transformer, regularization=1e-03):
         super().__init__()
-        self.save_hyperparameters({'l2 reg': lambda_})
+        self.save_hyperparameters({'l2 reg': regularization})
         self.model = BPNNModel()
         self.model.initialize_weights(example_tensormap)
         self.loss_fn = EnergyForceLoss(w_forces=True, force_weight=0.95)
         self.energy_transformer = energy_transformer
-        self.lambda_ = lambda_
+        self.regularization = regularization
 
     def forward(self, feats, systems):
         return self.model(feats, systems)
@@ -39,7 +39,7 @@ class BPNNRascalineModule(pl.LightningModule):
         self.log('train_loss', loss, enable_graph=True, batch_size=batch_size)
 
         for param in self.parameters():
-            loss += self.lambda_ * torch.norm(param)**2
+            loss += self.regularization * torch.norm(param)**2
 
         return loss
 
