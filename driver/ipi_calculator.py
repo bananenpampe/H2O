7608f235ed726b06097b28af22fb740b8fab53fd
Matthias Kellner <bananenpampe@t-online.de>
2023-08-15 17:37:49 +0200

    adds virial to calc

--- driver/ipi_calculator.py
+++ driver/ipi_calculator.py
@@ -26,6 +26,8 @@ import torch
 import rascaline
 from dataset.dataset import create_rascaline_dataloader, RascalineAtomisticDataset
 import ase.io
+from equistore.torch import Labels
+from torch.autograd import grad
 
 torch.set_default_dtype(torch.float64)
 
@@ -73,7 +75,7 @@ class PytorchLightningCalculator:
         calc_ps = rascaline.torch.SoapPowerSpectrum(**hypers_ps)
 
         self.dataset = RascalineAtomisticDataset(self.atoms,
-                                         energy_key="TotEnergy",
+                                         energy_key="potential",
                                          forces_key="force",
                                          calculators=[calc_rs, calc_ps],
                                          do_gradients=True,
@@ -85,30 +87,28 @@ class PytorchLightningCalculator:
         ex_frame = rascaline.torch.systems_to_torch(copy.deepcopy(self.atoms))
         feat = self.dataset._compute_feats(ex_frame, self.dataset.all_species)
 
-        transformer = CompositionTransformer()
-
         self.model = BPNNRascalineModule(\
         example_tensormap=feat,\
-        energy_transformer=transformer,\
         model=BPNNModel(\
         interaction=BPNNInteraction(n_out=1, activation=torch.nn.SiLU, n_hidden=64)))
-
+        
+        print(self.model.state_dict().keys())
         # ----- load model from checkpoint -----
-
+        
         if "energy_transformer.weights" in checkpoint.keys():
             print("found transformer weights")
+            transformer_weights = checkpoint.pop("energy_transformer.weights")
         else:
             print("setting transformer weights to 0.")
             
-            checkpoint["energy_transformer.weights"] = torch.nn.Parameter(torch.tensor([0. for i in self.dataset.all_species]).reshape(-1,1),
+            transformer_weights = torch.nn.Parameter(torch.tensor([0. for i in self.dataset.all_species]).reshape(-1,1),
                                                                     requires_grad=False)
             
-            self.model.energy_transformer.weights = checkpoint["energy_transformer.weights"]
-            
-            
+        self.model.energy_transformer.weights = transformer_weights 
         self.model.load_state_dict(checkpoint)
         self.model.energy_transformer.is_fitted = True
-
+        self.model.energy_transformer.unique_labels = Labels(["species_center"], values=torch.tensor(self.dataset.all_species).reshape(-1,1))
+        print(self.model)
 
 
     def calculate(self, positions, cell_matrix):
@@ -117,7 +117,8 @@ class PytorchLightningCalculator:
         self.atoms.set_cell(cell_matrix)
 
         forward_frame = rascaline.torch.systems_to_torch(self.atoms,
-                                                         positions_requires_grad=True)
+                                                         positions_requires_grad=True,
+                                                         cell_requires_grad=True)
         
         feat_forward = self.dataset._compute_feats(forward_frame,
                                                    self.dataset.all_species)
@@ -138,7 +139,15 @@ class PytorchLightningCalculator:
         energy = energy.detach().numpy()
         forces = forces.detach().numpy()
 
-        # for now return 0.0 virial
-        virial = cell_matrix * 0.0 
+        outputs = list(torch.ones_like(out.block(0).values))
 
-        return energy, forces, virial
+        dEdc = grad(outputs=list(out.block(0).values),
+                      inputs=[forward_frame.cell],
+                      grad_outputs=outputs,
+                      create_graph=True,
+                      retain_graph=True)[0]
+        
+        virial = -dEdc.detach().numpy().T @ cell_matrix
+        virial = 0.5 * (virial + virial.T)
+
+        return energy, forces, virial 
